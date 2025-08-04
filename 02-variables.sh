ea7346421675c69b361dd5cb64d5df549b45b51d
adityakonada <adityakonada@yahoo.com>
2025-08-04 10:07:28 +0200

    added variables file

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
-echo "Ava: Hey Sam, long time no see! How have you been?"
-echo "Sam: I’ve been good, just super busy with work. You?"
-echo "Ava: Same here! We should totally catch up sometime."
-echo "Sam: Absolutely. Coffee this weekend?"
-echo "Ava: Perfect! Saturday afternoon?"
-echo "Sam: Done. Can’t wait to hear everything!"
+PERSON1=adi
+PERSON2=konada
+echo "$PERSON1: Hey $PERSON2, long time no see! How have you been?"
+echo "$PERSON2: I’ve been good, just super busy with work. You?"
+echo "$PERSON1: Same here! We should totally catch up sometime."
+echo "$PERSON2: Absolutely. Coffee this weekend?"
+echo "$PERSON1: Perfect! Saturday afternoon?"
+echo "$PERSON2: Done. Can’t wait to hear everything!"
