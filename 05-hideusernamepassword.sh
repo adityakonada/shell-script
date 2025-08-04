c5b4064bfe7167814d0f12ed4bcb796c4439e3e2
adityakonada <adityakonada@yahoo.com>
2025-08-04 12:43:50 +0200

    added variables file

--- 05-hideusernamepassword.sh
+++ 05-hideusernamepassword.sh
@@ -2,10 +2,11 @@
 
 echo "please enter your username"
 
-read USERNAME #the value entered above will be automatically attached to username variable
+read -s USERNAME 
+#the value entered above will be automatically attached to username variable
 
 echo "please enter your password"
-read PASSWORD
+read -s PASSWORD
 
 echo "User name is : $USERNAME"
 echo "Password is : $PASSWORD"
