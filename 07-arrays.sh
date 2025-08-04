2b7e069860c0f274f5ee9ef2ea4deb8f7f9919cb
adityakonada <adityakonada@yahoo.com>
2025-08-04 13:40:41 +0200

    added arrays as fruits

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FRUITBASKET=("Mango" "pomegrante" "Avacado") 
+
+echo "First fruit in basket is ${FRUITBASKET[0]}"
+echo "Second fruit in basket is ${FRUITBASKET[1]}"
+echo "Third fruit in basket is ${FRUITBASKET[2]}"
+
+echo "All fruits in basket are: ${FRUITBASKET[@]}"
