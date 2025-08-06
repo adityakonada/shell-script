1b7f8729eb8a2dd7d8f483afcee77d5d79989307
adityakonada <adityakonada@yahoo.com>
2025-08-06 13:10:53 +0200

    ID

--- 09-MySQL.sh
+++ 09-MySQL.sh
@@ -5,9 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "You are not root user. Please run script with root access man"
-    exit 7 # you are asking program to exit from here, do not process further. 
+    exit 7 # you are asking program to exit from here, do not process further. You can give any number other than 1
 else
      echo "You are Root user"
 fi #fi means reverse of if condition, indicating to end if condition.
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR:: Installing MySQL is failed"
+    exit 7
+else
+    echo "MySQL installing is success"
