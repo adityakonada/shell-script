534ccc643e643315d94513f9229547b48354b255
adityakonada <adityakonada@yahoo.com>
2025-08-06 11:46:56 +0200

    ID

--- 09-MySQL.sh
+++ 09-MySQL.sh
@@ -7,4 +7,6 @@ then
     echo "You are not root user. Please run script with root access man"
 else
      echo "You are Root user"
-fi
+fi #fi means reverse of if condition, indicating to end if condition.
+
+dnf install mysql -y
