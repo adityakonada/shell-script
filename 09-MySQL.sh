e416a1902c1b33757248c93a90f7ebb77cf9a8bf
adityakonada <adityakonada@yahoo.com>
2025-08-06 13:05:33 +0200

    ID

--- 09-MySQL.sh
+++ 09-MySQL.sh
@@ -5,6 +5,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "You are not root user. Please run script with root access man"
+    exit 7 # you are asking program to exit from here, do not process further. 
 else
      echo "You are Root user"
 fi #fi means reverse of if condition, indicating to end if condition.
