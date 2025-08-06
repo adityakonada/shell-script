ee05da2552cbc74dc4afbab8425161e6dde2e74a
adityakonada <adityakonada@yahoo.com>
2025-08-06 13:33:24 +0200

    ID

---
+++ 10-MySQL,GIT.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+    echo "You are not root user. Please run script with root access man"
+    exit 7 # you are asking program to exit from here, do not process further. You can give any number other than 0
+else
+     echo "You are Root user"
+fi #fi means reverse of if condition, indicating to end if condition.
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR:: Installing MySQL is failed"
+    exit 1 #random number other than 0
+else
+    echo "MySQL installing is success"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "ERROR:: Installing GIT is failed"
+    exit 1 #random number other than 0
+else
+    echo "GIT installing is success"
+fi
