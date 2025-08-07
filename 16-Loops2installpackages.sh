51945a355ae579af11dfebc5145dd0ed0a4f87d7
adityakonada <adityakonada@yahoo.com>
2025-08-07 15:31:22 +0200

    adding loop package installation

---
+++ 16-Loops2installpackages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FILE="/tmp/$0-$TIMESTAMP.log" # --> $0 = file_name-date-time.log --> In this format the name of file.log will be stored 
+
+echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE # &>> is rediction concept
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2 installation $R error $N"
+        exit 1
+    else
+        echo "$2 installation $G success $N"
+    fi
+}
+
+
+
+if [ $ID -ne 0 ]
+then
+    echo -e "You are $R not root user $N. Please run script with root access man"
+    exit 1 # you are asking program to exit from here, do not process further.
+            # You can give any number other than 0
+else
+     echo -e "$G You are Root user"
+fi #fi means reverse of if condition, indicating to end if condition.
+
+dnf install $@
+
+VALIDATE $? "GIT"
